a172bcded7e7e0b46bb9fb5727fdc36218e70167
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-04-26 18:36:31 +0200

    Add helper method

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -217,7 +217,38 @@ type templateData struct {
 	Data    interface{}
 }
 
-func listObjectsByPrefix(prefix string) []string {
+func listFirstObjectByPrefix(prefix string) (string, error) {
+
+	log.Info("listing first in:", prefix)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// List objects
+	objectCh := minioClient.ListObjects(ctx, mediaBucket, minio.ListObjectsOptions{
+		Prefix:    prefix,
+		Recursive: false,
+		MaxKeys:   1,
+	})
+
+	ret := ""
+
+	for object := range objectCh {
+		if object.Err != nil {
+			log.Error(object.Err)
+			return "", object.Err
+		}
+
+		log.Info(object.Key)
+		ret = strings.TrimPrefix(object.Key, prefix)
+		log.Info("returening ", ret)
+		break
+	}
+	return ret, nil
+}
+
+func listObjectsByPrefix(prefix string) ([]string, error) {
+
 	log.Info("listing:", prefix)
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -233,25 +264,29 @@ func listObjectsByPrefix(prefix string) []string {
 	for object := range objectCh {
 		if object.Err != nil {
 			log.Error(object.Err)
-			return ret
+			return ret, object.Err
 		}
 		ret = append(ret, strings.TrimPrefix(strings.TrimSuffix(object.Key, "/"), prefix))
 	}
-	// log.Info(ret)
-	return ret
+	return ret, nil
 }
 
 func indexHandler(c *gin.Context) {
+
+	albums, err := getAlbumsByUsername(c.GetString("username"))
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
 
 	td := templateData{
 
 		Context: c,
 		Data: struct {
 			Title  string
-			Albums []string
+			Albums []Album
 		}{
 			Title:  "Albums",
-			Albums: listObjectsByPrefix(c.GetString("username") + "/"),
+			Albums: albums,
 		},
 	}
 
